d76180ebd6f892a33f8f60deb405e1a8c7be6084
kevin_smrt <kevinsanmartin@live.fr>
2021-12-19 21:06:40 +0100

    add cards data

---
+++ components/cards/cards.data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    title: "House of Cards",
+    price: 2.09,
+    imgURL:
+      "https://images.unsplash.com/photo-1630692929609-1fbfb477602e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80",
+  },
+  {
+    id: 2,
+    title: "Spectre",
+    price: 1.45,
+    imgURL:
+      "https://images.unsplash.com/photo-1618172193763-c511deb635ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+  },
+  {
+    id: 3,
+    title: "Present Tense",
+    price: 3,
+    imgURL:
+      "https://images.unsplash.com/photo-1632201717104-b7c59bc026ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 4,
+    title: "Starlight Hunter",
+    price: 9.01,
+    imgURL:
+      "https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 5,
+    title: "Soul Snachers",
+    price: 13.8,
+    imgURL:
+      "https://images.unsplash.com/photo-1626544827763-d516dce335e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80",
+  },
+  {
+    id: 6,
+    title: "Ideoteque",
+    price: 0.99,
+    imgURL:
+      "https://images.unsplash.com/photo-1627507055227-dd9c87118eb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+  },
+];
