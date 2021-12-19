d7dcb790409afc4112d51e1ebd700d9e6b159b15
kevin_smrt <kevinsanmartin@live.fr>
2021-12-19 21:05:29 +0100

    add section discover

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+// Components
 import Layout from "../components/Layout";
 
-export default function Home() {
-    return (
-        <Layout title="SoundCoin - Sell or Buy music NFT">
+// Sections
+import Discover from "../sections/Discover";
 
-        </Layout>
-    )
+export default function Home() {
+  return (
+    <Layout title="SoundCoin - Sell or Buy music NFT">
+      <Discover />
+    </Layout>
+  );
 }
